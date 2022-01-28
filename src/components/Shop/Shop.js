abd67fabc115a069ca5b4bb63f82b7458e39f634
Amin-Suaad <suaad.eee.aust@gmail.com>
2022-01-28 23:07:07 +0100

    shop page coding done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,6 +6,7 @@ import './Shop.css'
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10)
+    console.log(first10)
     const [products, setProducts] = useState(first10);
     const [cart, setCart] = useState([]);
     
