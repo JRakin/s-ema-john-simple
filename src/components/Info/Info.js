3cf18bd059f46c35b5b8d190ec0168643bc415f9
JRakin <juaidrakin1@gmail.com>
2022-02-07 22:04:21 +0600

    api integrated

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,49 +1,93 @@
-import React, { useEffect, useState } from 'react';
-import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import React, { useEffect, useState } from "react";
+import fakeData from "../../fakeData";
+import { getDatabaseCart, processOrder } from "../../utilities/databaseManager";
 
 const Info = () => {
-    const [cart, setCart] = useState([]);
-    const [userInfo, setUserInfo] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [orderPlaced, setOrderPlaced] = useState(false);
 
-    const handleBlur = (e) => {
-        const newInfo =  {name:e.target.name, value: e.target.value};
-        const newUserInfo = [...userInfo, newInfo];
-        setUserInfo(newUserInfo);
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
 
+    const cartProducts = productKeys.map((key) => {
+      const product = fakeData.find((pd) => pd.key === key);
+      product.quantity = savedCart[key];
+      return product;
+    });
+
+    setCart(cartProducts);
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const rawResponse = await fetch("http://localhost:5000/addOrder", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        address: address,
+        cart: cart,
+      }),
+    });
+    const content = await rawResponse.json();
+
+    console.log(content);
+
+    if (content) {
+      processOrder();
+      setOrderPlaced(true);
     }
-    console.log(userInfo);
+  };
 
-    useEffect(() => {
-        const savedCart = getDatabaseCart();
-        const productKeys = Object.keys(savedCart);
-
-        const cartProducts = productKeys.map(key => {
-            const product = fakeData.find(pd => pd.key === key);
-            product.quantity = savedCart[key];
-            return product;
-        });
-
-        setCart(cartProducts);
-    }, []);
-    return (
-        <div style = {{marginLeft: "100px"}}>
-        <h4>Please, fill out this form!</h4>
-        <form>
-            <input type="text" name= "Name" onBlur = {handleBlur} placeholder = "Name" />
-            <br />
-            <input type="text" name= "Email" onBlur = {handleBlur} placeholder = "Email" />
-            <br />
-            <input style = {{width: "500px"}} name= "Address" type="text" onBlur = {handleBlur} placeholder = "Address" />
-            <br />
-            <br />
-            <input type="submit" value="Submit" />
-            
-
-
-        </form>
-        </div>
-    );
+  return (
+    <div style={{ marginLeft: "100px" }}>
+      <h4>Please, fill out this form!</h4>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          name="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onBlur={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <br />
+        <input
+          required
+          type="text"
+          name="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onBlur={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <br />
+        <input
+          required
+          style={{ width: "500px" }}
+          name="Address"
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          onBlur={(e) => setAddress(e.target.value)}
+          placeholder="Address"
+        />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Info;
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,69 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import fakeData from '../../fakeData';
-import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
-import Cart from '../Cart/Cart';
-import ReviewItem from '../ReviewItem/ReviewItem';
-import happyImage from '../../images/giphy.gif'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import fakeData from "../../fakeData";
+import {
+  getDatabaseCart,
+  processOrder,
+  removeFromDatabaseCart,
+} from "../../utilities/databaseManager";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import happyImage from "../../images/giphy.gif";
+import { Link } from "react-router-dom";
 
 const Review = () => {
-    const [cart, setCart] = useState([]);
-    const [orderPlaced, setOrderPlaced] = useState(false);
+  const [cart, setCart] = useState([]);
+  const [orderPlaced, setOrderPlaced] = useState(false);
 
-    //handling when user clicks on the Place Order button
-    const handlePlaceOrder = () => {
-        const afterRemove = getDatabaseCart();
-        console.log(afterRemove);
-        setCart([]);
-        processOrder();
-        setOrderPlaced(true);
+  //handling when user clicks on the Place Order button
+  const handlePlaceOrder = () => {
+    const afterRemove = getDatabaseCart();
+    console.log(afterRemove);
+    setCart([]);
+    processOrder();
+    setOrderPlaced(true);
+  };
 
-    }
+  //removes a product
+  const removeProduct = (productKey) => {
+    const newCart = cart.filter((pd) => pd.key !== productKey);
+    setCart(newCart);
+    removeFromDatabaseCart(productKey);
+  };
 
-    //removes a product
-    const removeProduct = (productKey) => {
-        const newCart = cart.filter(pd => pd.key !== productKey);
-        setCart(newCart);
-        removeFromDatabaseCart(productKey);
-    }
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
 
-    
-    useEffect(() => {
-        const savedCart = getDatabaseCart();
-        const productKeys = Object.keys(savedCart);
+    const cartProducts = productKeys.map((key) => {
+      const product = fakeData.find((pd) => pd.key === key);
+      product.quantity = savedCart[key];
+      return product;
+    });
 
-        const cartProducts = productKeys.map(key => {
-            const product = fakeData.find(pd => pd.key === key);
-            product.quantity = savedCart[key];
-            return product;
-        });
+    setCart(cartProducts);
+  }, []);
 
-        setCart(cartProducts);
-    }, []);
+  return (
+    <div className="twin-container">
+      <div className="product-container">
+        {cart.map((pd) => (
+          <ReviewItem
+            removeProduct={removeProduct}
+            key={pd.key}
+            product={pd}
+          ></ReviewItem>
+        ))}
+        {orderPlaced && (
+          <h3 style={{ marginLeft: "200px", color: "#4BB543" }}>
+            Please, send your address details at shipping@ema-john.com
+          </h3>
+        )}
+        {orderPlaced && (
+          <img
+            style={{ marginLeft: "200px", marginTop: "5px" }}
+            src={happyImage}
+            alt=""
+          />
+        )}
+      </div>
 
-    return (
-        <div className="twin-container">
-            <div className="product-container">
-                {
-                    cart.map(pd => <ReviewItem removeProduct={removeProduct} key={pd.key} product={pd}></ReviewItem>)
-                }
-                {
-                    orderPlaced && <h3 style = {{marginLeft: "200px", color: "#4BB543"}}>Please, send your address details at shipping@ema-john.com</h3>
-                }
-                {
-                    orderPlaced && <img style={{ marginLeft: "200px", marginTop: "5px" }} src={happyImage} alt="" />
-                }
-            </div>
-
-            <div className="cart-container">
-                <Cart cart={cart}>
-                    <Link to = "/info">
-                    <button className="mainButton" onClick={handlePlaceOrder}>Place Order</button>
-                    </Link>
-                </Cart>
-            </div>
-        </div>
-    );
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <Link to="/info">
+            <button className="mainButton">Place Order</button>
+          </Link>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
